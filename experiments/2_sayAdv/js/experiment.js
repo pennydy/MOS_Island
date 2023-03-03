044efb206c967cabe7917c30919c057371c56425
pennydy <dpan@vassar.edu>
2023-03-03 10:56:58 -0800

    fix questionnaire

--- experiments/2_sayAdv/js/experiment.js
+++ experiments/2_sayAdv/js/experiment.js
@@ -241,13 +241,13 @@ function make_slides(f) {
         submit : function(e){
         //if (e.preventDefault) e.preventDefault(); // I don't know what this means.
         exp.subj_data = {
-          name : $("#name").val(),
+          // name : $("#name").val(),
           language : $("#language").val(),
           american : $('input[name="ame"]:checked').val(),
           enjoyment : $("#enjoyment").val(),
           asses : $('input[name="assess"]:checked').val(),
           age : $("#age").val(),
-          // fairprice : $("#fairprice").val(),
+          fairprice : $("#name").val(),
           gender : $("#gender").val(),
           education : $("#education").val(),
           problems : $("#problems").val(),
